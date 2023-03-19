561c00239865aaa0a420ef4d20fe4d748013bc21
Kev Fr <100141000+ClassyHarpy@users.noreply.github.com>
2023-03-19 22:00:05 +0100

    Basic Design

--- view/IndexController/chatAktion.tpl.php
+++ view/IndexController/chatAktion.tpl.php
@@ -1,21 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="view\IndexController\chatAktion.css">
     <script src="ajax.js"></script>
     <title>Chat</title>
 </head>
+
 <body>
-    <div id="chatBox" style="width: 300px; height: 400px; padding: 0.1rem 0.5rem 0rem 0.5rem; background-color: beige;">
-    
+    <div id="bar">
+        <div id="title">Chat</div>
     </div>
-    <input type="text" id="username" name="username" placeholder="username" required></br>
-    <textarea id="text" name="text" placeholder="Chatte" required></textarea>
+    <div id="chatBox">
+        <!--<div id="messages"></div>-->
+    </div>
+    <input type="text" id="username" name="username" placeholder="Benutzername" required></br>
+    <textarea id="text" name="text" placeholder="Nachricht eingeben" required></textarea>
     <button onclick="sende()">Senden</button>
     <!--
         <button onclick="getChats()">GET</button>
     -->
 </body>
+
 </html>
